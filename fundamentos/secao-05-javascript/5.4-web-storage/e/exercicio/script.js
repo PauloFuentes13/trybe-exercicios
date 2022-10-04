765318eae77ee4768cdb1e12b0d5d8fe8624097e
PauloFuentes13 <paulofuentes@outlook.com>
2022-10-04 09:57:05 -0300

    Exercicios 5.4 finalizado

--- fundamentos/secao-05-javascript/5.4-web-storage/e/exercicio/script.js
+++ fundamentos/secao-05-javascript/5.4-web-storage/e/exercicio/script.js
@@ -1,12 +1,17 @@
 window.onload = function(){
-    let elementSelect = document.querySelector('select');
+
+    let corMenu = document.getElementById('oi');
+
+    let elementSelect = document.querySelector('#colorSelect');
     elementSelect.addEventListener('change', function(){
         document.body.style.backgroundColor = elementSelect.value;
         
         localStorage.setItem('backgroundcolor', elementSelect.value);
+        corMenuWhite();
+        corMenuBlack();
     });
 
-    let elementSelectText = document.querySelectorAll('select')[1];
+    let elementSelectText = document.querySelector('#fontSelect');
     let text = document.getElementById('text');
     elementSelectText.addEventListener('change', function(){
         text.style.color = elementSelectText.value;
@@ -21,6 +26,33 @@ window.onload = function(){
         localStorage.setItem('size-font', `${fontSize1.value}px`);
     })
 
+    let lineHeight1 = document.getElementById('line');
+    lineHeight1.addEventListener('change', function(){
+        text.style.lineHeight = `${lineHeight1.value}%`;
+
+        localStorage.setItem('line-height', `${lineHeight1.value}%`);
+    })
+
+    let familyFo = document.getElementById('fam');
+    familyFo.addEventListener('change', function(){
+        text.style.fontFamily = `${familyFo.value}`;
+
+        localStorage.setItem('font-family', `${familyFo.value}`);
+    })
+     
+    function corMenuBlack(){
+        if (localStorage.getItem('backgroundcolor') === 'white'){
+            corMenu.style.color = 'black';
+        }
+    }
+
+    function corMenuWhite(){
+        if (localStorage.getItem('backgroundcolor') === 'black'){
+            corMenu.style.color = 'white';
+        }
+    }
+    
+
     let recuperaCorPag = localStorage.getItem('backgroundcolor');
     document.body.style.backgroundColor = recuperaCorPag;
     
@@ -29,4 +61,18 @@ window.onload = function(){
     
     let recuperaTamanhoFont = localStorage.getItem('size-font');
     text.style.fontSize = recuperaTamanhoFont;
+
+    let recuperaEspacamentoLine = localStorage.getItem('line-height');
+    text.style.lineHeight = recuperaEspacamentoLine;
+
+    let recuperaFamily = localStorage.getItem('font-family');
+    text.style.fontFamily = recuperaFamily;
+
+    
+}
+
+function colorMenu() {
+    if (recuperaCorPag === 'black') {
+        recuperaCorText = 'white';
+    }
 }
