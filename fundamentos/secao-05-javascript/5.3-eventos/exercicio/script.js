4e335d3f8ad7de3583c9e66f9ef65c592c15c8d5
PauloFuentes13 <paulofuentes@outlook.com>
2022-10-04 10:50:21 -0300

    Exercicios 5.3 parte 2

--- fundamentos/secao-05-javascript/5.3-eventos/exercicio/script.js
+++ fundamentos/secao-05-javascript/5.3-eventos/exercicio/script.js
@@ -12,6 +12,16 @@ function createDaysOfTheWeek() {
 }
   
   createDaysOfTheWeek();
+//   Crie um calendário dinamicamente.
+
+// O array decemberDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Sua função deve criar dinamicamente cada dia do calendário e os adicionar dentro da tag <ul>.
+
+// Note que os dias 29 e 30 de novembro estão no array, pois representam respectivamente Domingo e Segunda-feira.
+
+// A tag <ul> deve conter o id 'days'
+// Os dias devem estar contidos em uma tag <li>, e todos devem ter a classe day. Ex: <li class="day">3</li>
+// Os dias 24, 25 e 31 são feriados e, além da classe day, devem conter também a classe holiday. Ex: <li class="day holiday">24</li>
+// Os dias 4, 11, 18 e 25 são sextas-feiras. Eles devem conter a classe day e a classe friday. Ex: <li class="day friday">4</li>
   
   // Escreva seu código abaixo.
 
@@ -37,4 +47,32 @@ function createDays() {
     }
 }
 createDays();
-  
+
+// Implemente uma função que crie dinamicamente um botão com o nome “Feriados”.
+// Sua função deve receber um parâmetro com a string 'Feriados'
+// Adicione a este botão a ID "btn-holiday"
+// Adicione este botão como filho/filha da tag <div> com classe "buttons-container"
+
+const divButtons = document.querySelector('.buttons-container');
+// console.log(divButtons);
+
+function buttonHoliday(name) {
+    const buttonHoli = document.createElement('button');
+    let idButtons = 'btn-holiday';
+
+    buttonHoli.id = idButtons;
+    buttonHoli.innerHTML = name;
+
+    divButtons.appendChild(buttonHoli);
+}
+buttonHoliday('Feriados');
+
+
+
+
+
+
+
+
+
+
